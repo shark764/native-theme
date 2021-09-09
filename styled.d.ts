68862be1b6b146e5b6997aba9dc0c65ab07bed39
Farid Hernandez <farid.hdz.64@gmail.com>
2021-09-08 21:33:59 -0600

    refactor: Removing uneeded packages

--- styled.d.ts
+++
@@ -1,6 +0,0 @@
-import {} from 'styled-components';
-import { DefaultThemeProps } from './src/types';
-
-declare module 'styled-components' {
-  export interface DefaultTheme extends DefaultThemeProps {}
-}
